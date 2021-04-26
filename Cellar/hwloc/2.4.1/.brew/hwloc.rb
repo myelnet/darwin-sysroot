6cf501ded85d7c39b14d59d9d3cb59e21ab81e77
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2021-04-26 14:13:32 +0100

    Init commit
    - Darwin sysroot for cross compilation using goreleaser

---
+++ Cellar/hwloc/2.4.1/.brew/hwloc.rb
@@ -0,0 +1,41 @@
+class Hwloc < Formula
+  desc "Portable abstraction of the hierarchical topology of modern architectures"
+  homepage "https://www.open-mpi.org/projects/hwloc/"
+  url "https://www.open-mpi.org/software/hwloc/v2.4/downloads/hwloc-2.4.1.tar.bz2"
+  sha256 "392421e69f26120c8ab95d151fe989f2b4b69dab3c7735741c4e0a6d7de5de63"
+  license "BSD-3-Clause"
+
+  head do
+    url "https://github.com/open-mpi/hwloc.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+
+  uses_from_macos "libxml2"
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--enable-shared",
+                          "--enable-static",
+                          "--prefix=#{prefix}",
+                          "--disable-cairo",
+                          "--without-x"
+    system "make", "install"
+
+    pkgshare.install "tests"
+
+    # remove homebrew shims directory references
+    rm Dir[pkgshare/"tests/**/Makefile"]
+  end
+
+  test do
+    system ENV.cc, pkgshare/"tests/hwloc/hwloc_groups.c", "-I#{include}",
+                   "-L#{lib}", "-lhwloc", "-o", "test"
+    system "./test"
+  end
+end
